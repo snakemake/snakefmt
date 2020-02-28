6169b9a05f9311e0338d0791a0bd1c8618ef7a08
Michael Hall <michael@mbh.sh>
2020-02-28 16:15:05 +0000

    add line length option

--- snakefmt/snakefmt.py
+++ snakefmt/snakefmt.py
@@ -1,8 +1,21 @@
 import click
 
+DEFAULT_LINE_LENGTH = 88
+
+
 @click.command()
+@click.help_option("--help", "-h")
+@click.option(
+    "-l",
+    "--line-length",
+    default=DEFAULT_LINE_LENGTH,
+    show_default=True,
+    type=click.IntRange(min=1),
+)
 def main():
+    """The uncompromising Snakemake code formatter."""
     print("Hello world")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
