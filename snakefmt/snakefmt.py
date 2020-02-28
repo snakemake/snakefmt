c35503003add32af4191090dec3a527ce3abff31
Michael Hall <michael@mbh.sh>
2020-02-28 16:39:35 +0000

    add version and src params

--- snakefmt/snakefmt.py
+++ snakefmt/snakefmt.py
@@ -1,20 +1,23 @@
+from typing import List
 import click
+from snakefmt import __version__
 
 DEFAULT_LINE_LENGTH = 88
 
 
 @click.command()
 @click.help_option("--help", "-h")
+@click.version_option(__version__, "--version", "-V")
 @click.option(
-    "-l",
-    "--line-length",
-    default=DEFAULT_LINE_LENGTH,
-    show_default=True,
-    type=click.IntRange(min=1),
+    "-l", "--line-length", default=DEFAULT_LINE_LENGTH, show_default=True, type=int,
 )
-def main():
+@click.argument(
+    "src", nargs=-1, type=click.Path(exists=True, writable=True, allow_dash=True)
+)
+def main(line_length: int, src: List[str]):
     """The uncompromising Snakemake code formatter."""
-    print("Hello world")
+    for p in src:
+        print(f"Got file {p}")
 
 
 if __name__ == "__main__":
