9725ba1a2ed1624e230e88a278b9ba148b63f7ae
Michael Hall <michael@mbh.sh>
2020-02-29 11:37:07 +0000

    add tests for regex construction

--- tests/test_snakefmt.py
+++ tests/test_snakefmt.py
@@ -1,6 +1,36 @@
+import re
+import pytest
+
 from snakefmt.snakefmt import construct_regex
 
 
 class TestConstructRegex:
     def test_noNewline_returnsCompiledRegex(self):
-        regex = ""
+        regex = r"\.smk$"
+
+        actual = construct_regex(regex)
+        expected = re.compile(regex)
+
+        assert actual == expected
+
+    def test_containsNewline_returnsCompiledRegexWithMultilineSetting(self):
+        regex = r"""
+(
+  /(
+      \.eggs         # exclude a few common directories in the
+    | \.git          # root of the project
+    | \.snakemake
+  )/
+)
+"""
+
+        actual = construct_regex(regex)
+        expected = re.compile(regex, re.MULTILINE | re.VERBOSE)
+
+        assert actual == expected
+
+    def test_invalidRegex_raisesError(self):
+        regex = r"?"
+
+        with pytest.raises(re.error):
+            construct_regex(regex)
