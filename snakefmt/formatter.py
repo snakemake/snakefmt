37103dd3321db9f9a122a4bf9eb4c9305d4ce428
Michael Hall <mbhall88@gmail.com>
2020-03-16 16:31:41 +0000

    add factory method from pyproject for Formatter

--- snakefmt/formatter.py
+++ snakefmt/formatter.py
@@ -1,12 +1,35 @@
+from pathlib import Path
+
+import toml
+
 from snakefmt import DEFAULT_LINE_LENGTH
 
 
 class Formatter:
     """A class to control the formatting of a string/stream."""
 
-    def __init__(self, line_length: int = DEFAULT_LINE_LENGTH):
+    def __init__(self, line_length: int = DEFAULT_LINE_LENGTH) -> "Formatter":
         self._line_length = line_length
+
+    def __eq__(self, other: "Formatter") -> bool:
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
 
     @property
     def line_length(self) -> int:
         return self._line_length
+
+    @staticmethod
+    def from_config(config_path: Path) -> "Formatter":
+        config = toml.loads(config_path.read_text())
+        tools = config.get("tool", {})
+        snakefmt_opts = tools.get("snakefmt", {})
+        try:
+            line_length = int(snakefmt_opts["line_length"])
+        except KeyError:
+            line_length = int(
+                tools.get("black", {}).get("line_length", DEFAULT_LINE_LENGTH)
+            )
+        return Formatter(line_length=line_length)
--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from unittest.mock import patch
+
 from snakefmt.formatter import Formatter
 
 
@@ -9,3 +12,52 @@ def test_line_length():
     expected = line_length
 
     assert actual == expected
+
+
+class TestEquality:
+    def test_same_returnsTrue(self):
+        assert Formatter() == Formatter()
+
+    def test_different_returnsFalse(self):
+        assert not Formatter() == Formatter(line_length=0)
+
+
+class TestFromConfig:
+    @patch("pathlib.Path.read_text", return_value="")
+    def test_emptyConfig_returnsDefault(self, mock):
+        config = Path("pyproject.toml")
+
+        actual = Formatter.from_config(config)
+        expected = Formatter()
+
+        assert actual == expected
+
+    @patch("pathlib.Path.read_text", return_value="[tool.foo]\nbar=true")
+    def test_configWithNoSnakfmtSection_returnsDefault(self, mock):
+        config = Path("pyproject.toml")
+
+        actual = Formatter.from_config(config)
+        expected = Formatter()
+
+        assert actual == expected
+
+    @patch("pathlib.Path.read_text", return_value="[tool.black]\nline_length=1000")
+    def test_configWithOnlyBlackSection_returnsBlackParams(self, mock):
+        config = Path("pyproject.toml")
+
+        actual = Formatter.from_config(config)
+        expected = Formatter(line_length=1_000)
+
+        assert actual == expected
+
+    @patch(
+        "pathlib.Path.read_text",
+        return_value="[tool.black]\nline_length=1\n[tool.snakefmt]\nline_length=5",
+    )
+    def test_configWithSnakefmtAndBlackSection_returnsSnakefmtParams(self, mock):
+        config = Path("pyproject.toml")
+
+        actual = Formatter.from_config(config)
+        expected = Formatter(line_length=5)
+
+        assert actual == expected
