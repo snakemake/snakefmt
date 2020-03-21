0c1d453eddaa1b0244c147a37f6d65736ff1a11f
Michael Hall <mbhall88@gmail.com>
2020-03-21 18:13:29 +0000

    add tests with lambda functions as params [breaks]

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -36,3 +36,49 @@ class TestFormatter:
         expected = "# configfile: 'foo.yaml'\n"
 
         assert actual == expected
+
+    def test_simple_rule_one_input(self):
+        stream = StringIO("rule a:\n\tinput: 'foo.txt'")
+        smk = Snakefile(stream)
+        formatter = Formatter(smk)
+
+        actual = formatter.get_formatted()
+        expected = """rule a:
+\tinput: 
+\t\t\"foo.txt\" \n"""
+
+        assert actual == expected
+
+    def test_lambda_function_as_parameter(self):
+        stream = StringIO(
+            """rule a: 
+                input: lambda wildcards: foo(wildcards)"""
+        )
+        smk = Snakefile(stream)
+        formatter = Formatter(smk)
+
+        actual = formatter.get_formatted()
+        expected = """rule a:
+\tinput: 
+\t\tlambda wildcards: foo(wildcards) \n"""
+
+        assert actual == expected
+
+    def test_lambda_function_as_parameter_for_parameter(self):
+        stream = StringIO(
+            """rule a: 
+\tinput: 'foo.txt'
+\tresources:
+\t\tmem_mb = lambda wildcards, attempt: attempt * 1000"""
+        )
+        smk = Snakefile(stream)
+        formatter = Formatter(smk)
+
+        actual = formatter.get_formatted()
+        expected = """rule a:
+\tinput: 
+\t\t\"foo.txt\"
+\tresources:
+\t\tmem_mb = lambda wildcards, attempt: attempt * 1000,\n"""
+
+        assert actual == expected
