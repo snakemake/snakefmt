3178c66e79b1c7be9ccc83a6f6670d3183319654
Michael Hall <michael@mbh.sh>
2020-04-13 16:24:42 +0100

    add click callback to set defaults based on config file

--- tests/test_snakefmt.py
+++ tests/test_snakefmt.py
@@ -1,9 +1,11 @@
+import os
 import re
 import tempfile
 from collections import Counter
 from pathlib import Path
 from unittest import mock
 
+import click
 import pytest
 from click.testing import CliRunner
 
@@ -11,6 +13,7 @@ from snakefmt.snakefmt import (
     construct_regex,
     main,
     get_snakefiles_in_dir,
+    read_snakefmt_defaults_from_pyproject_toml,
 )
 
 
@@ -58,6 +61,170 @@ class TestCLI:
         actual = cli_runner.invoke(main, params)
         assert actual.exit_code != 0
         assert "Invalid regular expression" in str(actual.exception)
+
+
+class TestReadSnakefmtDefaultsFromPyprojectToml:
+    def test_no_value_passed_and_no_pyproject_changes_nothing(self, tmpdir):
+        os.chdir(tmpdir)
+        default_map = dict()
+        ctx = click.Context(click.Command("snakefmt"), default_map=default_map)
+        param = mock.MagicMock()
+        value = None
+
+        return_val = read_snakefmt_defaults_from_pyproject_toml(ctx, param, value)
+
+        assert return_val is None
+
+        actual_default_map = ctx.default_map
+        expected_default_map = dict()
+
+        assert actual_default_map == expected_default_map
+
+    def test_no_value_passed_and_pyproject_present_but_empty_changes_nothing_returns_pyproject_path(
+        self, tmpdir
+    ):
+        os.chdir(tmpdir)
+        pyproject = Path("pyproject.toml")
+        pyproject.touch()
+        default_map = dict()
+        ctx = click.Context(click.Command("snakefmt"), default_map=default_map)
+        param = mock.MagicMock()
+        value = None
+
+        actual_config_path = read_snakefmt_defaults_from_pyproject_toml(
+            ctx, param, value
+        )
+        expected_config_path = None
+
+        assert actual_config_path == expected_config_path
+
+        actual_default_map = ctx.default_map
+        expected_default_map = dict()
+
+        assert actual_default_map == expected_default_map
+
+    def test_no_value_passed_and_pyproject_present_changes_default_line_length(
+        self, tmpdir
+    ):
+        os.chdir(tmpdir)
+        pyproject = Path("pyproject.toml")
+        pyproject.write_text("[tool.snakefmt]\nline_length = 4")
+        default_map = dict(line_length=88)
+        ctx = click.Context(click.Command("snakefmt"), default_map=default_map)
+        param = mock.MagicMock()
+        value = None
+
+        actual_config_path = read_snakefmt_defaults_from_pyproject_toml(
+            ctx, param, value
+        )
+        expected_config_path = str(pyproject)
+
+        assert actual_config_path == expected_config_path
+
+        actual_default_map = ctx.default_map
+        expected_default_map = dict(line_length=4)
+
+        assert actual_default_map == expected_default_map
+
+    def test_no_value_passed_and_pyproject_present_unknown_param_adds_to_default_map(
+        self, tmpdir
+    ):
+        os.chdir(tmpdir)
+        pyproject = Path("pyproject.toml")
+        pyproject.write_text("[tool.snakefmt]\nfoo = true")
+        default_map = dict()
+        ctx = click.Context(click.Command("snakefmt"), default_map=default_map)
+        param = mock.MagicMock()
+        value = None
+
+        actual_config_path = read_snakefmt_defaults_from_pyproject_toml(
+            ctx, param, value
+        )
+        expected_config_path = str(pyproject)
+
+        assert actual_config_path == expected_config_path
+
+        actual_default_map = ctx.default_map
+        expected_default_map = dict(foo=True)
+
+        assert actual_default_map == expected_default_map
+
+    def test_value_passed_reads_from_path(self, tmpdir):
+        os.chdir(tmpdir)
+        pyproject = Path("snakefmt.toml")
+        pyproject.write_text("[tool.snakefmt]\nfoo = true")
+        default_map = dict()
+        ctx = click.Context(click.Command("snakefmt"), default_map=default_map)
+        param = mock.MagicMock()
+        value = str(pyproject)
+
+        actual_config_path = read_snakefmt_defaults_from_pyproject_toml(
+            ctx, param, value
+        )
+        expected_config_path = str(pyproject)
+
+        assert actual_config_path == expected_config_path
+
+        actual_default_map = ctx.default_map
+        expected_default_map = dict(foo=True)
+
+        assert actual_default_map == expected_default_map
+
+    def test_value_passed_but_default_map_is_empty_still_updates_defaults(self, tmpdir):
+        os.chdir(tmpdir)
+        pyproject = Path("snakefmt.toml")
+        pyproject.write_text("[tool.snakefmt]\nfoo = true")
+        default_map = None
+        ctx = click.Context(click.Command("snakefmt"), default_map=default_map)
+        param = mock.MagicMock()
+        value = str(pyproject)
+
+        actual_config_path = read_snakefmt_defaults_from_pyproject_toml(
+            ctx, param, value
+        )
+        expected_config_path = str(pyproject)
+
+        assert actual_config_path == expected_config_path
+
+        actual_default_map = ctx.default_map
+        expected_default_map = dict(foo=True)
+
+        assert actual_default_map == expected_default_map
+
+    def test_value_passed_in_overrides_pyproject(self, tmpdir):
+        os.chdir(tmpdir)
+        snakefmt_config = Path("snakefmt.toml")
+        snakefmt_config.write_text("[tool.snakefmt]\nfoo = true")
+        pyproject = Path("pyproject.toml")
+        pyproject.write_text("[tool.snakefmt]\n\nfoo = false\nline_length = 90")
+        default_map = dict()
+        ctx = click.Context(click.Command("snakefmt"), default_map=default_map)
+        param = mock.MagicMock()
+        value = str(snakefmt_config)
+
+        actual_config_path = read_snakefmt_defaults_from_pyproject_toml(
+            ctx, param, value
+        )
+        expected_config_path = str(snakefmt_config)
+
+        assert actual_config_path == expected_config_path
+
+        actual_default_map = ctx.default_map
+        expected_default_map = dict(foo=True)
+
+        assert actual_default_map == expected_default_map
+
+    def test_malformatted_toml_raises_error(self, tmpdir):
+        os.chdir(tmpdir)
+        pyproject = Path("pyproject.toml")
+        pyproject.write_text("foo:bar,baz\n{dict}\&&&&")
+        default_map = dict()
+        ctx = click.Context(click.Command("snakefmt"), default_map=default_map)
+        param = mock.MagicMock()
+        value = None
+
+        with pytest.raises(click.FileError):
+            read_snakefmt_defaults_from_pyproject_toml(ctx, param, value)
 
 
 class TestConstructRegex:
