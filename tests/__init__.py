7b7f117cdcadcaea6f44d37fcf9f2b687512cfad
Michael Hall <michael@mbh.sh>
2020-04-13 18:25:51 +0100

    add function to read black config from pyproject

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,11 @@
 from io import StringIO
-from snakefmt.parser.parser import Snakefile
+
+from snakefmt import DEFAULT_LINE_LENGTH
 from snakefmt.formatter import Formatter
+from snakefmt.parser.parser import Snakefile
 
 
-def setup_formatter(snake: str):
+def setup_formatter(snake: str, line_length: int = DEFAULT_LINE_LENGTH):
     stream = StringIO(snake)
     smk = Snakefile(stream)
-    return Formatter(smk)
+    return Formatter(smk, line_length=line_length)
