991fc36e65f51a0193660c7e1a1111ad8887fa7a
Brice Letcher <bletcher@ebi.ac.uk>
2020-09-09 13:40:41 +0100

    Make #65 solution python <3.8 compatible

--- snakefmt/__init__.py
+++ snakefmt/__init__.py
@@ -1,6 +1,11 @@
-from importlib import metadata
+import importlib_metadata
 
-# Fetches version from distribution metadata files deriving from pyproject.toml
-__version__ = metadata.version("snakefmt")
+"""
+Version has unique source in pyproject.toml.
+importlib fetches version from distribution metadata files
+(in dist-info or egg-info dirs).
+From Python 3.8, importlib_metadata is in standard library as importlib.metadata.
+"""
+__version__ = importlib_metadata.version("snakefmt")
 
 DEFAULT_LINE_LENGTH = 88
