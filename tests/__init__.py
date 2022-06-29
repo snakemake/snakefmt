4c83e0692c1613f718671c414396a58aafe8bd5c
Michael Hall <michael@mbh.sh>
2022-06-29 14:35:06 +1000

    fix: do not align the inside of multiline strings [#123]]

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,13 @@
+import logging
 from io import StringIO
 
 from snakefmt.formatter import Formatter
+from snakefmt.logging import LogConfig
 from snakefmt.parser.parser import Snakefile
 
 
 def setup_formatter(snake: str, line_length: int = None, black_config_file=None):
     stream = StringIO(snake)
     smk = Snakefile(stream)
+    LogConfig.init(logging.DEBUG)
     return Formatter(smk, line_length=line_length, black_config_file=black_config_file)
