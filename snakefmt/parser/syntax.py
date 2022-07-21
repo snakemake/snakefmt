90e45b760f3fe9fe7bcf126f8f1a042f074e5c66
Johannes Köster <johannes.koester@uni-due.de>
2022-07-21 10:48:53 +0200

    shorten lines

--- snakefmt/parser/syntax.py
+++ snakefmt/parser/syntax.py
@@ -246,13 +246,14 @@ class KeywordSyntax(Syntax):
         identifier = r"[a-zA-Z_]\S*"
         use_syntax_regexp = (
             r"use rule (?:(?:{id})|\*)"
-            r"(?: from {id})?(?: exclude {id}( *, *{id})*)?(?: as {id})?( with[ ]?:)?$".format(
-                id=identifier
-            )
-        )
+            r"(?: from {id})?(?: exclude {id}( *, *{id})*)?"
+            r"(?: as {id})?( with[ ]?:)?$"
+        ).format(id=identifier)
         use_ebnf_syntax = (
             '"use" "rule" (identifier | "*") '
-            '"from" identifier ["exclude" identifier {"," identifier}] ["as" identifier] ["with" ":"]'
+            '"from" identifier '
+            '["exclude" identifier {"," identifier}] '
+            '["as" identifier] ["with" ":"]'
         )
         while not is_newline(self.token):
             if self.token.type == tokenize.COMMENT:
