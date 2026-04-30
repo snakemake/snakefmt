8ed21c8a557059e58968093180e69b41f10f52f0
Michael Hall <mhaldini@icloud.com>
2026-04-30 10:23:37 +1000

    fix(nvim): use single BufWritePre autocmd for auto-format

--- lua/snakefmt/init.lua
+++ lua/snakefmt/init.lua
@@ -3,19 +3,15 @@ local M = {}
 function M.setup(opts)
   config.setup(opts)
   if config.options.auto_format then
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "snakemake",
-      callback = function(args)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = args.buf,
-          callback = function()
-            M.format()
-          end,
-          desc = "Auto-format Snakemake file on save",
-        })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = { "*.smk", "Snakefile" },
+      callback = function()
+        M.format()
       end,
+      desc = "Auto-format with snakefmt on save",
     })
   end
+
   vim.api.nvim_create_user_command("Snakefmt", function()
     M.format()
   end, { desc = "Format current buffer with snakefmt" })
