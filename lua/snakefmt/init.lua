de555956ad11ace802e1e2cb8c3c3889bf8167c7
Michael Hall <mhaldini@icloud.com>
2026-04-30 10:30:25 +1000

    feat(nvim): implement async formatting using vim.system

--- lua/snakefmt/init.lua
+++ lua/snakefmt/init.lua
@@ -1,4 +1,5 @@
 local config = require("snakefmt.config")
+local paths = require("snakefmt.paths")
 local M = {}
 
 local group = vim.api.nvim_create_augroup("snakefmt", { clear = true })
@@ -21,8 +22,40 @@ function M.setup(opts)
     M.format()
   end, { desc = "Format current buffer with snakefmt" })
 end
+
 function M.format()
-  -- To be implemented in next tasks
-  print("Snakefmt triggered")
+  local cmd = paths.get_snakefmt_bin(config.options)
+  if not cmd then
+    vim.notify("snakefmt: binary not found and uvx fallback failed", vim.log.levels.ERROR)
+    return
+  end
+
+  local bufnr = vim.api.nvim_get_current_buf()
+  local content = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local stdin = table.concat(content, "\n") .. "\n"
+
+  -- Build command args (make a copy of cmd table to avoid modifying original)
+  local full_cmd = vim.list_extend({}, cmd)
+  table.insert(full_cmd, "-")
+  if config.options.line_length then
+    table.insert(full_cmd, "--line-length")
+    table.insert(full_cmd, tostring(config.options.line_length))
+  end
+
+  vim.system(full_cmd, { stdin = stdin }, function(obj)
+    vim.schedule(function()
+      if obj.code == 0 then
+        local formatted = vim.split(obj.stdout, "\n")
+        if formatted[#formatted] == "" then
+          table.remove(formatted)
+        end
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, formatted)
+        vim.notify("snakefmt: formatted", vim.log.levels.INFO)
+      else
+        vim.notify("snakefmt error: " .. obj.stderr, vim.log.levels.ERROR)
+      end
+    end)
+  end)
 end
+
 return M
