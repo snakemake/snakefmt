0e9789e5417f9c0ebb871363789103998fbdb6a7
Michael Hall <mhaldini@icloud.com>
2026-04-30 10:22:02 +1000

    fix(nvim): address code review issues in Task 1

--- lua/snakefmt/init.lua
+++ lua/snakefmt/init.lua
@@ -5,19 +5,20 @@ function M.setup(opts)
   if config.options.auto_format then
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "snakemake",
-      callback = function()
+      callback = function(args)
         vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = 0,
+          buffer = args.buf,
           callback = function()
             M.format()
           end,
+          desc = "Auto-format Snakemake file on save",
         })
       end,
     })
   end
   vim.api.nvim_create_user_command("Snakefmt", function()
     M.format()
-  end, {})
+  end, { desc = "Format current buffer with snakefmt" })
 end
 function M.format()
   -- To be implemented in next tasks
